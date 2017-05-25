f9a3b423cff2a3ec6c5d0b5d2ce0eefc71bed03b
heshamelmasry77 <heshamelmasry@outlook.com>
2017-05-25 11:36:24 +0200

    Setting up an env variable , changing variables, adding logic to prod and dev, adding sass style expanded and compressed not working still

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,43 @@ var concat = require('gulp-concat');
 var compass = require('gulp-compass');
 var connect = require('gulp-connect');
 
+var env,
+    coffeeSources,
+    jsSources,
+    sassSources,
+    htmlSources,
+    jsonSources,
+    outputDir,
+    sassStyle;
+
+
+env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+    outputDir = 'builds/development/';
+    sassStyle = 'expanded';
+} else {
+    outputDir = 'builds/production/';
+    sassStyle = 'compressed';
+
+
+}
 
 // any file with an extension of coffee
-var coffeeSources = ['components/coffee/*.coffee'];
+coffeeSources = ['components/coffee/*.coffee'];
 
-var jsSources = [
+jsSources = [
     'components/scripts/rclick.js',
     'components/scripts/pixgrid.js',
     'components/scripts/tagline.js',
     'components/scripts/template.js'
 ];
 
-var sassSources = ['components/sass/style.scss'];
+sassSources = ['components/sass/style.scss'];
 
-var htmlSources = ['builds/development/*.html'];
+htmlSources = [outputDir + '*.html'];
+
+jsonSources = [outputDir + '*.json'];
 gulp.task('coffee', function () {
     gulp.src(coffeeSources)
         .pipe(coffee({bare: true})
@@ -32,7 +55,7 @@ gulp.task('js', function () {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
-        .pipe(gulp.dest('builds/development/js'))
+        .pipe(gulp.dest(outputDir + 'js'))
         .pipe(connect.reload())
 
 });
@@ -41,11 +64,11 @@ gulp.task('compass', function () {
     gulp.src(sassSources)
         .pipe(compass({
             sass: 'components/sass',
-            image: 'builds/development/images',
-            style: 'expanded'
+            image: outputDir + 'images',
+            style: sassStyle
         })
             .on('error', gutil.log))
-        .pipe(gulp.dest('builds/development/css'))
+        .pipe(gulp.dest(outputDir + 'css'))
         .pipe(connect.reload())
 
 
@@ -57,13 +80,13 @@ gulp.task('watch', function () {
     gulp.watch(coffeeSources, ['coffee']);
     gulp.watch('components/sass/*.scss', ['compass']);
     gulp.watch(htmlSources, ['html']);
-    gulp.watch('builds/development/js/*.json', ['json']);
+    gulp.watch(jsonSources, ['json']);
 });
 
 // task to start up the server
 gulp.task('connect', function () {
     connect.server({
-        root: 'builds/development/',
+        root: outputDir,
         livereload: true
     });
 });
@@ -76,9 +99,9 @@ gulp.task('html', function () {
 });
 // task to watch and reload when i change any json files
 gulp.task('json', function () {
-    gulp.src('builds/development/js/*.json')
+    gulp.src(jsonSources)
         .pipe(connect.reload())
 
 });
 
-gulp.task('default', ['coffee', 'js','json', 'compass', 'watch', 'connect', 'html']);
+gulp.task('default', ['coffee', 'js', 'json', 'compass', 'watch', 'connect', 'html']);
